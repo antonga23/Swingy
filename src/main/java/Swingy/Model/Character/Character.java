a70a9880e6235396f482a144379365e2d4d77614
antonga <antonga@student.wethinkcode.co.za>
2019-08-02 17:18:12 +0200

    Revert "Revert "did more more work on heros and artifacts""
    This reverts commit 193a9581

---
+++ src/main/java/Swingy/Model/Character/Character.java
@@ -0,0 +1,12 @@
+package Swingy.Model.Character;
+
+public abstract class Character {
+
+    protected String heroName;
+    protected String heroClass;
+    protected int level;
+    protected int experience;
+    protected int attack;
+    protected int defense;
+    protected int hitPoints;
+}
